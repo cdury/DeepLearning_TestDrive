a02acaf4dbc7e10d47a0a8711654711ae479a146
BIGWIN10\christian <christian@dury.org>
2019-06-28 16:27:33 +0200

    New Nework EEG_deepLSTM

---
+++ categorical/EEGEyes/deepLSTM/NeuralNetwork.py
@@ -0,0 +1,189 @@
+# general imports
+import os
+import copy
+import logging
+from categorical.helper.profiling import timing
+import numpy as np
+import pandas as pd
+import sklearn.metrics as skm
+import matplotlib.pyplot as plt
+
+# typing imports
+from typing import Tuple, Union
+from numpy import ndarray
+from pandas import DataFrame, Series
+from scipy.sparse import csr_matrix
+
+# keras imports
+from tensorflow.python.keras.api._v2 import keras
+
+# # Backend
+keras_backend = keras.backend
+# # Layers
+Input = keras.layers.Input
+Dense = keras.layers.Dense
+LSTM = keras.layers.LSTM
+GaussianNoise = keras.layers.GaussianNoise
+# # Model
+Model = keras.models.Model
+Sequential = keras.models.Sequential
+# # Regularizers
+l2 = keras.regularizers.l2
+# # Optimizer
+Adam = keras.optimizers.Adam
+Adadelta = keras.optimizers.Adadelta
+# # Utils
+plot_model = keras.utils.plot_model
+
+dir_name = os.path.split(os.path.split(os.path.dirname(__file__))[0])[1]
+sub_dir_name = model_name = os.path.split(os.path.dirname(__file__))[1]
+model_name = dir_name + "_" + sub_dir_name
+dir_path = os.path.join(dir_name,sub_dir_name)
+
+# helper imports
+
+# Network
+import categorical.model.deepLSTM as deepLSTM
+
+# Data
+from categorical.EEGEyes.eeg_eyes import Loader
+
+logger = logging.getLogger(os.path.splitext(os.path.basename(__file__))[0])
+
+##############################################################################################
+# Parameters
+##############################################################################################
+
+
+class HyperParameters(deepLSTM.NNParameters):
+    """ HyperParamters for the neural network
+
+    """
+
+    def __init__(self, model_name, loglevel, parent_name=None):
+        # Process parameter
+        super().__init__(model_name, dir_path, loglevel, parent_name)
+        # Loader
+        loader = Loader()
+        # # Data
+        self.colum_names = copy.deepcopy(loader.input_signal_types)
+        self.labels = copy.deepcopy(loader.label)
+        self.categorizations = copy.deepcopy(loader.classes)
+
+        # Hyperparameter
+        self.label = self.labels
+        self.classes = self.categorizations  # [self.label]
+        # # Training (Hyperparameters)
+        self.batch_size = 4
+        self.epochs = 50
+        # END Hyperparameter
+
+        del loader
+
+
+##############################################################################################
+# Neural Network
+# API
+##############################################################################################
+
+
+class DeepLearning(deepLSTM.NNDefinition):
+    def __init__(self, hyperparameter):
+        super().__init__(hyperparameter)
+        self.parameter: HyperParameters = hyperparameter
+
+    @timing
+    def load_data(
+        self
+    ) -> Tuple[
+        Tuple[Union[DataFrame, Series, ndarray, csr_matrix], ...],
+        Tuple[Union[DataFrame, Series, ndarray, csr_matrix], ...],
+        Tuple[Union[DataFrame, Series, ndarray, csr_matrix], ...],
+        Tuple[Union[DataFrame, Series, ndarray, csr_matrix], ...],
+    ]:
+        # Import data
+        loader = Loader()
+        x_train, y_train, x_train_pred, y_train_pred, x_val, y_val, x_test, y_test = loader.eeg_data(
+            self.parameter.batch_size
+        )
+
+        train_data = x_train, y_train
+        train_data_pred = x_train_pred, y_train_pred
+        test_data = x_test, y_test
+        valid_data = x_val, y_val
+        self.train_data = train_data
+        self.test_data = test_data
+        self.validation_data = valid_data
+        return train_data, train_data_pred, test_data, valid_data
+
+    def calc_categorical_accuracy(self, model, non_train_data, add_data=None):
+        train_data_pred = add_data
+        final_metrics = {}
+        trainX_forPred, trainY_forPred = train_data_pred
+        y_train_pred = model.predict(
+            np.repeat(
+                trainX_forPred, self.parameter.batch_size, axis=0
+            )  ## need same input shape as for training and thus repeating 4 times
+        )
+        ## score
+        final_metrics["accuracy"] = skm.accuracy_score(
+            trainY_forPred[0, :, 0], np.where(y_train_pred[0, :, 0] > 0.8, 1, 0)
+        )
+
+        ## evaluate first batch (other are copies)
+        plt.plot(trainY_forPred[0, :, 0], label="true")
+        plt.plot(y_train_pred[0, :, 0], label="pred0", alpha=0.5)
+        plt.plot(y_train_pred[0, :, 1], label="pred1", alpha=0.5)
+        plt.legend()
+        plt.show()
+
+        testX, testY = non_train_data
+        # evaulate test ste
+        y_pred = model.predict(np.repeat(testX, self.parameter.batch_size, axis=0))
+        ## score
+        final_metrics["val_accuracy"] = skm.accuracy_score(
+            testY[0, :, 0], np.where(y_pred[0, :, 0] > 0.8, 1, 0)
+        )
+        plt.plot(testY[0, :, 0], label="true")
+        plt.plot(y_pred[0, :, 0], label="pred0", alpha=0.5)
+        plt.plot(y_pred[0, :, 1], label="pred1", alpha=0.5)
+        plt.legend()
+        plt.show()
+
+    def setup_and_train_network(self):
+        # Data
+        # # Loading
+        train_data, train_data_pred, test_data, valid_data = self.load_data()
+
+        # Model
+        # # Definition
+        model = self.define_model(train_data[0].shape, train_data[1].shape)
+
+        # # Training
+        model, final_metrics, label_vectors, training_history = self.train_network(
+            epochs=self.parameter.epochs, accuracy_data=train_data_pred
+        )
+
+        return model, final_metrics, label_vectors, training_history
+
+
+if __name__ == "__main__":
+    # Information printout
+    from tensorflow.python.client import device_lib
+
+    logger.debug("ENVIRONMENT")
+    for device in device_lib.list_local_devices():
+        logger.debug(
+            f"{device.name}:{device.device_type} with memory {device.memory_limit}"
+        )
+        logger.debug(f" {device.physical_device_desc}")
+
+    # Define Hyperparameters
+    hyperparameters = HyperParameters(model_name=model_name, loglevel=logging.DEBUG)
+    # Setup Model
+    neural_network = DeepLearning(hyperparameters)
+    # Train model
+    neural_network.setup_and_train_network()
+    # Do more stuff
+    # neural_network.train_network(100)
+    logger.debug("Finished")
---
+++ categorical/EEGEyes/eeg_eyes.py
@@ -0,0 +1,164 @@
+# This dataloader loads the "motionsense-dataset"
+# http://archive.ics.uci.edu/ml/datasets/EEG+Eye+State
+#
+# Usage:
+# from dataloader.motionsense import Loader
+# dataloader = Loader()
+# trainX, trainY, trainX_forPred, trainY_forPred, valX, valY, testX, testY = dataloader.eeg_eyes(<batches>)
+import os
+import re
+import glob
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+
+# Only for debugging
+import matplotlib.pyplot as plt
+
+# typing imports
+from typing import Tuple, List, Any, Union, Optional
+from numpy import ndarray
+from pandas import DataFrame, Series
+from scipy.sparse import csr_matrix
+
+DEBUG = False
+
+
+class Loader:
+    def __init__(self):
+        self.home_path = os.getcwd()
+        self.data_path = os.path.join("data")
+        self.dataset_path = os.path.join(self.home_path, self.data_path, "EEG")
+        self.filename = r"EEG Eye State.arff"
+        # Labels of input columns
+        self.input_signal_types = [
+            "AF3",
+            "F7",
+            "F3",
+            "FC5",
+            "T7",
+            "P7",
+            "O1",
+            "O2",
+            "P8",
+            "T8",
+            "FC6",
+            "F4",
+            "F8",
+            "AF4",
+        ]
+        # Output classes to learn how to classify
+        self.label = "eyeDetection"
+        self.classes = ["EyeClosed", "EyeOpen"]
+
+        #
+        ## train-test-splitt:
+        ## define point in time t0 where we split the time series
+        ## train is before t0
+        ## test is after t0
+        self.t0 = 12000
+
+        ## ts_length
+        ## timeseries 0-t0 itd aufgeteilt in
+        ## kleinere timeserie der Länge ts_length
+        ## alle ts_length/batches fängt eine neue kleine timeseries an
+        ## d.h. 1.timeseries   0 - 2000 (0 - ts_length)
+        ##      2.timeseries 500 - 2500 (1 * ts_length/batches - (1 * ts_length/batches +ts_length) )
+        ##      ...
+        self.ts_length = 2000
+
+    def eeg_data(self, batches):
+        """Numpy Array of input and labels for training and test data
+
+        :param batches: Number of batches
+        :return: trainX, trainY, trainX_forPred, trainY_forPred, valX, valY, testX, testY
+        """
+
+        def prep_time_data(start, end):
+            X1 = np.expand_dims(X[start:end, :], axis=0)
+            Y1 = y[start:end].reshape(-1, 1)
+            Y1 = np.expand_dims(np.hstack([Y1, 1 - Y1]), axis=0)
+            return X1, Y1
+
+        assert (
+            self.ts_length / batches == self.ts_length // batches
+        ), "batches must divide ts_length"
+        data = pd.read_csv(
+            os.path.join(self.dataset_path, self.filename),
+            names=self.input_signal_types + [self.label] ,
+            skiprows=19,
+            index_col=False,
+        )
+
+        # PREPARE DATA
+        X = data[self.input_signal_types].values
+        y = data[self.label].values
+
+        scaler = preprocessing.RobustScaler()
+        scaler.fit(X[: self.t0, :])
+        X = scaler.transform(X)
+        le = preprocessing.LabelEncoder()
+        y = le.fit_transform(y)
+        ## cut signal at -1 and 1
+        # X = X/2 ## alternative: cut at -2 and 2 and scale
+        idx1 = X > 1
+        idx2 = X < -1
+        X_peaks = np.zeros_like(X)
+        X[idx1] = 1
+        X[idx2] = -1
+        ## and add new columns which indicate if there was a peak
+        X_peaks[idx1] = 1
+        X_peaks[idx2] = -1
+        X = np.hstack([X, X_peaks])
+
+        ## divide dataset into step_size segments,
+        #  each segment corrsesponds to one of the batches
+        step_size = self.ts_length // batches  # 1000
+
+        n_ts = self.t0 // self.ts_length - 1
+        first_start = self.t0 - (n_ts + 1) * self.ts_length # = 0
+        ts_starts = np.array(
+            range(first_start, self.t0 - step_size - self.ts_length + 1, step_size)
+        )
+        windows = pd.DataFrame(
+            {
+                "batch": list(range(batches)) * n_ts,
+                "start": ts_starts,
+                "end": ts_starts + self.ts_length,
+            }
+        )
+        ## build training set
+        X_list = []
+        Y_list = []
+        for start in range(
+            first_start, self.t0 - step_size - self.ts_length + 1, step_size
+        ):
+            X1, Y1 = prep_time_data(start, start + self.ts_length)
+            X_list.append(X1)
+            Y_list.append(Y1)
+        # put into 3D Tensor (number of small time series, lenght of small time series, inputs)
+        trainX = np.vstack(X_list)
+        trainY = np.vstack(Y_list)
+
+        ## validation set to observe while training
+        X_list = []
+        Y_list = []
+        for i in range(1 - batches, 1):
+            start = self.t0 + i * step_size
+            X1, Y1 = prep_time_data(start, start + self.ts_length)
+            X_list.append(X1)
+            Y_list.append(Y1)
+
+        valX = np.vstack(X_list)
+        valY = np.vstack(Y_list)
+
+        ## test set
+        n_pred = self.ts_length  # 2000
+        trainX_forPred, trainY_forPred = prep_time_data(
+            self.t0 - n_pred, self.t0
+        )  ## used to generate valid state
+        testX, testY = prep_time_data(self.t0, self.t0 + n_pred)
+
+        return trainX, trainY, trainX_forPred, trainY_forPred, valX, valY, testX, testY
